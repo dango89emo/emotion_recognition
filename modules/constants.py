2b41a2c4b4ec82388ab0e76a8381856f0dba8160
dangoMM <ocean.in.the.valley@gmail.com>
2022-08-03 21:03:39 +0900

    hide personal data

--- modules/constants.py
+++ modules/constants.py
@@ -1,9 +1,13 @@
 class Path:
-    AUDIO_FOLDER = "/Users/t_yousuke/Desktop/audio-dataset/*"
-    OUTPUT_FOLDER_TRAIN = "/Users/t_yousuke/Desktop/output_folder_train/"
-    OUTPUT_FOLDER_TEST = "/Users/t_yousuke/Desktop/output_folder_test/"
+    AUDIO_FOLDER = "audio-dataset/*"
+    OUTPUT_FOLDER_TRAIN = "output_folder_train/"
+    OUTPUT_FOLDER_TEST = "output_folder_test/"
 
 model_name = "audio_emotion_vit.pth"
 img_size = 256
 
 classes = ['angry', 'calm', 'disgust', 'fearful', 'happy', 'neutral', 'sad', 'surprised']
+
+class AWSConfig:
+    bucket="" # 自分のバケット名を入力
+    region="ap-northeast-1"
